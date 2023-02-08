6eddf80a9f4540d352e8e942690018d14e6e1ad6
harshithdhara291 <harshithdhara291@gmail.com>
2023-02-08 09:26:16 +0000

    initial commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,8 @@ import {Link, withRouter} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import {HiLocationMarker, HiTruck} from 'react-icons/hi'
 import {BsPersonFill, BsHeart} from 'react-icons/bs'
-import {AiOutlineShoppingCart} from 'react-icons/ai'
+import {AiOutlineShoppingCart, AiOutlineCaretDown} from 'react-icons/ai'
+import {FaPaw, FaRupeeSign} from 'react-icons/fa'
 
 import CartContext from '../../context/CartContext'
 
@@ -52,7 +53,7 @@ const Header = props => {
         </div>
         <div className="inr">
           <button className="btn" type="button">
-            INR ^
+            INR <AiOutlineCaretDown />
           </button>
           <ul className="drop-menu">
             <li>INR</li>
@@ -61,111 +62,109 @@ const Header = props => {
         </div>
       </div>
       <nav className="nav-header">
-        <div className="nav-content">
-          <div className="nav-bar-mobile-logo-container">
-            <Link to="/">
-              <img
-                className="website-logo"
-                src="https://fable.club/wp-content/uploads/2022/11/Fable-Club.svg"
-                alt="website logo"
-              />
-            </Link>
-
-            <button
-              type="button"
-              className="nav-mobile-btn"
-              onClick={onClickLogout}
-            >
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
-                alt="nav logout"
-                className="nav-bar-img"
-              />
-            </button>
-          </div>
-
-          <div className="nav-bar-large-container">
-            <Link to="/">
-              <img
-                className="website-logo"
-                src="	https://fable.club/wp-content/uploads/2022/11/Fable-Club.svg"
-                alt="website logo"
-              />
-            </Link>
-            <ul className="nav-menu">
-              <li className="nav-menu-item">
-                <Link to="/" className="nav-link">
-                  Home
-                </Link>
-              </li>
-
-              <li className="nav-menu-item">
-                <Link to="/" className="nav-link">
-                  Shop
-                </Link>
-              </li>
-
-              <li className="nav-menu-item">
-                <Link to="/" className="nav-link">
-                  Pages
-                </Link>
-              </li>
-
-              <li className="nav-menu-item">
-                <Link to="/" className="nav-link">
-                  Blog
-                </Link>
-              </li>
-
-              <li className="nav-menu-item">
-                <Link to="/" className="nav-link">
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
-            <div className="nav-ends">
-              <input type="search" />
-              <BsPersonFill />
-              <BsHeart />
-              <Link to="/cart" className="nav-link">
-                <AiOutlineShoppingCart />
-                {renderCartItemsCount()}
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="nav-menu-mobile">
-          <ul className="nav-menu-list-mobile">
-            <li className="nav-menu-item-mobile">
+        <div className="nav-bar-large-container">
+          <Link to="/">
+            <img
+              className="website-logo"
+              src="	https://fable.club/wp-content/uploads/2022/11/Fable-Club.svg"
+              alt="website logo"
+            />
+          </Link>
+          <ul className="nav-menu">
+            <li className="nav-menu-item">
               <Link to="/" className="nav-link">
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-                  alt="nav home"
-                  className="nav-bar-img"
-                />
+                Home
               </Link>
             </li>
 
-            <li className="nav-menu-item-mobile">
-              <Link to="/products" className="nav-link">
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
-                  alt="nav products"
-                  className="nav-bar-img"
-                />
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Shop <AiOutlineCaretDown />
               </Link>
+              <ul className="drop-menu-shop">
+                <li className="list-item">
+                  <FaPaw /> Shop – Left Sidebar
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Shop – Right Sidebar
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Shop – Listview
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Shop – Full Width 5 Columns
+                </li>
+              </ul>
             </li>
-            <li className="nav-menu-item-mobile">
-              <Link to="/cart" className="nav-link">
-                <img
-                  src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-                  alt="nav cart"
-                  className="nav-bar-img"
-                />
-                {renderCartItemsCount()}
+
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Pages <AiOutlineCaretDown />
+              </Link>
+              <ul className="drop-menu-1">
+                <li className="list-item">
+                  <FaPaw /> About US
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Faqs
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Icons
+                </li>
+                <li className="list-item">
+                  <FaPaw /> 404 Error
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Blog <AiOutlineCaretDown />
+              </Link>
+              <ul className="drop-menu-1">
+                <li className="list-item">
+                  <FaPaw /> Blog - Standard{' '}
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Blog - Grid 2 Column
+                </li>
+                <li className="list-item">
+                  <FaPaw /> Single Post
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-menu-item">
+              <Link to="/" className="nav-link">
+                Contact
               </Link>
             </li>
           </ul>
+          <div className="nav-ends">
+            <input
+              type="search"
+              placeholder="Search products..."
+              className="search"
+            />
+            <BsPersonFill className="last-icon" />
+            <BsHeart className="last-icon" />
+            <Link to="/cart" className="nav-link">
+              <AiOutlineShoppingCart className="cart-icon" />
+              {renderCartItemsCount()}
+            </Link>
+            <h1 className="head-1">
+              <FaRupeeSign /> 0.00
+            </h1>
+          </div>
+        </div>
+        <div className="mobile">
+          <Link to="/">
+            <img
+              className="mobile-logo"
+              src="	https://fable.club/wp-content/uploads/2022/11/Fable-Club.svg"
+              alt="website logo"
+            />
+          </Link>
         </div>
       </nav>
     </>
